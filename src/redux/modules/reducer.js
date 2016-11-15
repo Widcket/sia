0a3995f91fcac2c5dc2143955ecc39c33bad225d
Rita Zerrizuela <zeta@widcket.com>
2016-11-15 19:14:58 -0300

    Added reducers to combinator function

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,17 +1,21 @@
-import multireducer from 'multireducer';
-import {routerReducer} from 'react-router-redux';
 import {combineReducers} from 'redux';
-import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 import {reducer as form} from 'redux-form';
-
-import info from './info';
-import widgets from './widgets';
+// import multireducer from 'multireducer';
+import navigation from './navigation';
+import {reducer as reduxAsyncConnect} from 'redux-async-connect';
+import {routerReducer} from 'react-router-redux';
+import step1 from './step1';
+import step2 from './step2';
+import step3 from './step3';
+import step4 from './step4';
 
 export default combineReducers({
     routing: routerReducer,
     reduxAsyncConnect,
     form,
-    multireducer: multireducer({}),
-    info,
-    widgets
+    step1,
+    step2,
+    step3,
+    step4,
+    navigation
 });
